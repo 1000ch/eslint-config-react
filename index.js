d928fcd6fc50dcdb14fc196929f28f2ecbe1dcb6
1000ch <shogo.sensui@gmail.com>
2016-09-13 15:51:23 +0900

    Update plugins and related rules

--- index.js
+++ index.js
@@ -15,30 +15,33 @@ module.exports = {
     // List of supported rules
     // ++++++++++++++++++++
     'react/display-name'                 : 'off', // Prevent missing displayName in a React component definition
+    'react/forbid-component-props'       : 'off', // Forbid certain props on Components
     'react/forbid-prop-types'            : 'off', // Forbid certain propTypes
-    'react/no-comment-textnodes'         : 'off', // Prevent comments from being inserted as text nodes
     'react/no-danger'                    : 'off', // Prevent usage of dangerous JSX properties
+    'react/no-danger-with-children'      : 'off', // Prevent problem with children and props.dangerouslySetInnerHTML
     'react/no-deprecated'                : 'error', // Prevent usage of deprecated methods
     'react/no-did-mount-set-state'       : 'error', // Prevent usage of setState in componentDidMount
     'react/no-did-update-set-state'      : 'error', // Prevent usage of setState in componentDidUpdate
     'react/no-direct-mutation-state'     : 'error', // Prevent direct mutation of this.state
+    'react/no-find-dom-node'             : 'off', // Prevent usage of findDOMNode
     'react/no-is-mounted'                : 'error', // Prevent usage of isMounted
     'react/no-multi-comp'                : 'error', // Prevent multiple component definition per file
     'react/no-render-return-value'       : 'off', // Prevent usage of the return value of React.render
     'react/no-set-state'                 : 'off', // Prevent usage of setState
-    'react/no-string-refs'               : 'off', // Prevent using string references in ref attribute.
+    'react/no-string-refs'               : 'off', // Prevent using string references in ref attribute
     'react/no-unknown-property'          : 'error', // Prevent usage of unknown DOM property (fixable)
+    'react/no-unused-prop-types'         : 'error', // Prevent definitions of unused prop types
     'react/prefer-es6-class'             : 'error', // Enforce ES5 or ES6 class for React Components
     'react/prefer-stateless-function'    : 'off', // Enforce stateless React Components to be written as a pure function
     'react/prop-types'                   : 'error', // Prevent missing props validation in a React component definition
     'react/react-in-jsx-scope'           : 'error', // Prevent missing React when using JSX
-    'react/require-extension'            : 'error', // Restrict file extensions that may be required
     'react/require-optimization'         : 'off', // Enforce React components to have a shouldComponentUpdate method
+    'react/no-comment-textnodes'         : 'off', // Prevent comments from being inserted as text nodes
     'react/require-render-return'        : 'error', // Enforce ES5 or ES6 class for returning value in render function
     'react/self-closing-comp'            : 'error', // Prevent extra closing tags for components without children
     'react/sort-comp'                    : 'error', // Enforce component methods order
     'react/sort-prop-types'              : 'off', // Enforce propTypes declarations alphabetical sorting
-    'react/wrap-multilines'              : 'error', // Prevent missing parentheses around multilines JSX (fixable)
+    'react/style-prop-object'            : 'off', // Enforce style prop value being an object
     // ++++++++++++++++++++
     // List of supported rules
     // ++++++++++++++++++++
@@ -54,6 +57,7 @@ module.exports = {
     'react/jsx-key'                      : 'off', // Validate JSX has key prop when in array or iterator
     'react/jsx-max-props-per-line'       : 'off', // Limit maximum of props on a single line in JSX
     'react/jsx-no-bind'                  : 'error', // Prevent usage of .bind() and arrow functions in JSX props
+    'react/jsx-no-comment-textnodes'     : 'error', // Prevent comments from being inserted as text nodes
     'react/jsx-no-duplicate-props'       : 'error', // Prevent duplicate props in JSX
     'react/jsx-no-literals'              : 'off', // Prevent usage of unwrapped JSX strings
     'react/jsx-no-target-blank'          : 'error', // Prevent usage of unsafe target='_blank'
@@ -63,5 +67,6 @@ module.exports = {
     'react/jsx-space-before-closing'     : ['error', 'always'], // Validate spacing before closing bracket in JSX (fixable)
     'react/jsx-uses-react'               : 'error', // Prevent React to be incorrectly marked as unused
     'react/jsx-uses-vars'                : 'error' // Prevent variables used in JSX to be incorrectly marked as unused
+    'react/jsx-wrap-multilines'          : 'error' // Prevent missing parentheses around multilines JSX (fixable)
   }
 };
--- jsx-a11y.js
+++ jsx-a11y.js
@@ -11,10 +11,12 @@ module.exports = {
     }
   },
   rules: {
+    'anchor-has-content'           : 'error', // Enforce all anchors to contain accessible content.
     'aria-props'                   : 'error', // Enforce all aria-* props are valid.
     'aria-proptypes'               : 'error', // Enforce ARIA state and property values are valid.
     'aria-role'                    : 'error', // Enforce that elements with ARIA roles must use a valid, non-abstract ARIA role.
     'aria-unsupported-elements'    : 'error', // Enforce that elements that do not support ARIA roles, states, and properties do not have those attributes.
+    'click-events-have-key-events' : 'error', // Enforce a clickable non-interactive element has at least one keyboard event listener.
     'heading-has-content'          : 'error', // Enforce heading (h1, h2, etc) elements contain accessible content.
     'href-no-hash'                 : 'error', // Enforce an anchor element's href prop value is not just #.
     'html-has-lang'                : 'error', // Enforce <html> element has lang prop.
@@ -26,6 +28,7 @@ module.exports = {
     'no-access-key'                : 'error', // Enforce that the accessKey prop is not used on any element to avoid complications with keyboard 'commands used by a screenreader.
     'no-marquee'                   : 'error', // Enforce <marquee> elements are not used.
     'no-onchange'                  : 'error', // Enforce that onBlur is used instead of onChange.
+    'no-static-element-interactions': 'error', // Enforce non-interactive elements have no interactive handlers.
     'onclick-has-focus'            : 'error', // Enforce that elements with onClick handlers must be focusable.
     'onclick-has-role'             : 'error', // Enforce that non-interactive, visible elements (such as <div>) that have click handlers use the 'role attribute.
     'role-has-required-aria-props' : 'error', // Enforce that elements with ARIA roles must have all required attributes for that 'role.
